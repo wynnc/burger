945b7282f3a38a9416f2e27c45158b62c3167f6c
Wynn Chaney <wynn.chaney@wanco.com>
2019-07-29 08:03:47 -0600

    Created folder structure and needed files and logic.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', true);
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden', true);
+-- INSERT INTO burgers (burger_name) VALUES ('Owens');
